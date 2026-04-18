dbd8ec1c273a2672685987c5c26041f9aadc7fbc
Imeragajanayaka20 <imeragajanayaka20@gmail.com>
2026-04-19 05:00:27 +0530

    feat(supervisor): [Imera] add SupervisorResearchArea db context and run migration

---
+++ BlindMatchPAS.Core/Entities/SupervisorResearchArea.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlindMatchPAS.Core.Entities
+{
+    public class SupervisorResearchArea
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string SupervisorId { get; set; } = string.Empty;
+        public ApplicationUser Supervisor { get; set; } = null!;
+
+        [Required]
+        public int ResearchAreaId { get; set; }
+        public ResearchArea ResearchArea { get; set; } = null!;
+    }
+}
